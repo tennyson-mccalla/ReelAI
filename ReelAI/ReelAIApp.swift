7f7b5bb34a4ba92ce16820ed87cd96ff607abd51
Tennyson McCalla <tennyson.mccalla@gmail.com>
2025-02-03 13:06:12 -0600

    Initial Commit

---
+++ ReelAI/ReelAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReelAIApp.swift
+//  ReelAI
+//
+//  Created by ten dev on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ReelAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReelAITests/ReelAITests.swift
@@ -0,0 +1,17 @@
+//
+//  ReelAITests.swift
+//  ReelAITests
+//
+//  Created by ten dev on 2/3/25.
+//
+
+import Testing
+@testable import ReelAI
+
+struct ReelAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
